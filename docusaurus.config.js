ab4d51f57bfe492a80f43c32613c6d962e9cff88
Max Wolfs <mail@maxwolfs.com>
2023-09-20 14:10:51 +0200

    fix single quotes
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,86 +1,86 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "One platform — standardized, built and operated by many.",
-  tagline: "Documentation and Community Platform for the Sovereign Cloud Stack",
-  url: "https://docs.scs.community",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  title: 'One platform — standardized, built and operated by many.',
+  tagline: 'Documentation and Community Platform for the Sovereign Cloud Stack',
+  url: 'https://docs.scs.community',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
   markdown: {
     mermaid: true,
   },
-  organizationName: "SovereignCloudStack", // Usually your GitHub org/user name.
-  projectName: "docs", // Usually your repo name.
+  organizationName: 'SovereignCloudStack', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebarsDocs.js"),
-          editUrl: "https://github.com/SovereignCloudStack/docs/tree/main/",
+          sidebarPath: require.resolve('./sidebarsDocs.js'),
+          editUrl: 'https://github.com/SovereignCloudStack/docs/tree/main/',
         },
         blog: {
           showReadingTime: true,
-          editUrl: "https://github.com/SovereignCloudStack/docs/tree/main/",
+          editUrl: 'https://github.com/SovereignCloudStack/docs/tree/main/',
         },
         theme: {
-          customCss: [require.resolve("./src/css/custom.css")],
+          customCss: [require.resolve('./src/css/custom.css')],
         },
       }),
     ],
   ],
   plugins: [
     [
-      "@docusaurus/plugin-client-redirects",
+      '@docusaurus/plugin-client-redirects',
       {
         redirects: [
           {
-            to: "/docs/iaas/deployment-examples/testbed",
-            from: "/docs/category/osism-testbed/",
+            to: '/docs/iaas/deployment-examples/testbed',
+            from: '/docs/category/osism-testbed/',
           },
         ],
         createRedirects(existingPath) {
-          if (existingPath.includes("/community")) {
-            return [existingPath.replace("/community", "/community/community")];
+          if (existingPath.includes('/community')) {
+            return [existingPath.replace('/community', '/community/community')];
           }
           return undefined; // Return a falsy value: no redirect created
         },
       },
     ],
     [
-      "@docusaurus/plugin-content-docs",
+      '@docusaurus/plugin-content-docs',
       {
-        id: "community",
-        path: "community",
-        routeBasePath: "community",
-        sidebarPath: require.resolve("./sidebarsCommunity.js"),
+        id: 'community',
+        path: 'community',
+        routeBasePath: 'community',
+        sidebarPath: require.resolve('./sidebarsCommunity.js'),
         // ... other options
       },
     ],
     [
-      "@docusaurus/plugin-content-docs",
+      '@docusaurus/plugin-content-docs',
       {
-        id: "standards",
-        path: "standards",
-        routeBasePath: "standards",
-        sidebarPath: require.resolve("./sidebarsStandards.js"),
+        id: 'standards',
+        path: 'standards',
+        routeBasePath: 'standards',
+        sidebarPath: require.resolve('./sidebarsStandards.js'),
       },
     ],
-    "./src/plugins/docusaurus-plugin-matomo-analytics/index.js",
+    './src/plugins/docusaurus-plugin-matomo-analytics/index.js',
   ],
 
   themeConfig:
@@ -88,98 +88,98 @@ const config = {
     ({
       metadata: [
         {
-          title: "One platform — standardized, built and operated by many.",
+          title: 'One platform — standardized, built and operated by many.',
           description:
-            "Documentation and Community Platform for the Sovereign Cloud Stack",
+            'Documentation and Community Platform for the Sovereign Cloud Stack',
         },
       ],
-      image: "img/summit-social.png",
+      image: 'img/summit-social.png',
       navbar: {
-        title: "",
+        title: '',
         logo: {
-          alt: "SCS",
-          src: "img/logo.svg",
+          alt: 'SCS',
+          src: 'img/logo.svg',
         },
         items: [
           //   { to: '/blog', label: 'Blog', position: 'left' },
-          { to: "/docs", label: "Docs", position: "left" },
-          { to: "/standards", label: "Standards", position: "left" },
-          { to: "/community", label: "Community", position: "left" },
-          { to: "/docs/faq", label: "FAQ", position: "left" },
+          { to: '/docs', label: 'Docs', position: 'left' },
+          { to: '/standards', label: 'Standards', position: 'left' },
+          { to: '/community', label: 'Community', position: 'left' },
+          { to: '/docs/faq', label: 'FAQ', position: 'left' },
           {
-            href: "https://github.com/SovereignCloudStack/docs",
-            label: "GitHub",
-            position: "right",
+            href: 'https://github.com/SovereignCloudStack/docs',
+            label: 'GitHub',
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "light",
+        style: 'light',
         links: [
           {
-            title: "Docs",
+            title: 'Docs',
             items: [
               {
-                label: "Contribute",
-                to: "/docs",
+                label: 'Contribute',
+                to: '/docs',
               },
             ],
           },
           {
-            title: "Community",
+            title: 'Community',
             items: [
               {
-                label: "Matrix",
-                href: "https://matrix.to/#/!TiDqlLmEUaXqTemaLc:matrix.org?via=matrix.org",
+                label: 'Matrix',
+                href: 'https://matrix.to/#/!TiDqlLmEUaXqTemaLc:matrix.org?via=matrix.org',
               },
               {
-                label: "Mastodon",
-                href: "https://fosstodon.org/@sovereigncloudstack",
+                label: 'Mastodon',
+                href: 'https://fosstodon.org/@sovereigncloudstack',
               },
             ],
           },
           {
-            title: "More",
+            title: 'More',
             items: [
               // {
               //   label: 'Blog',
               //   to: '/blog'
               // },
               {
-                label: "GitHub",
-                href: "https://github.com/SovereignCloudStack/docs",
+                label: 'GitHub',
+                href: 'https://github.com/SovereignCloudStack/docs',
               },
             ],
           },
         ],
         copyright:
-          "Sovereign Cloud Stack, SCS and the logo are registered trademarks of the Open Source Business Alliance e.V. — Other trademarks are property of their respective owners.",
+          'Sovereign Cloud Stack, SCS and the logo are registered trademarks of the Open Source Business Alliance e.V. — Other trademarks are property of their respective owners.',
       },
       prism: {
         theme: darkCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ["powershell", "ruby"],
+        additionalLanguages: ['powershell', 'ruby'],
       },
       matomoAnalytics: {
-        matomoUrl: "https://matomo.scs.community/",
-        siteId: "2",
-        phpLoader: "matomo.php",
-        jsLoader: "matomo.js",
+        matomoUrl: 'https://matomo.scs.community/',
+        siteId: '2',
+        phpLoader: 'matomo.php',
+        jsLoader: 'matomo.js',
         disableCookies: true,
       },
     }),
 
   themes: [
-    "@docusaurus/theme-mermaid",
+    '@docusaurus/theme-mermaid',
     [
       // @ts-ignore
-      "@easyops-cn/docusaurus-search-local",
+      '@easyops-cn/docusaurus-search-local',
       /** @type {import('@easyops-cn/docusaurus-search-local').PluginOptions} */
       // @ts-ignore
       ({
         hashed: true,
-        docsDir: ["docs", "community", "standards"],
-        docsRouteBasePath: ["docs", "community", "standards"],
+        docsDir: ['docs', 'community', 'standards'],
+        docsRouteBasePath: ['docs', 'community', 'standards'],
       }),
     ],
   ],
