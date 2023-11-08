23e6479891f176a374c19d8fca9a348e353be419
Max Wolfs <mail@maxwolfs.com>
2023-11-08 16:35:05 +0100

    add content
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,61 +3,60 @@ import clsx from 'clsx'
 import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
-import HomepageFeatures from '@site/src/components/HomepageFeatures'
 
 import styles from './index.module.css'
 import ContentCard from '../components/ContentCard'
 
 const featureContentData = [
   {
-    title: 'Test the Reference Implementation',
-    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+    title: 'Introduction to SCS',
+    body: 'Get to know SCS better and learn about the background.',
     url: '/docs',
     buttonText: 'Get Started'
   },
   {
-    title: 'Upgrade Guides',
-    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
-    url: '/docs',
-    buttonText: 'Dive Deeper'
+    title: 'Releases',
+    body: 'SCS is currently in Release 5. Check out the latest Release Notes.',
+    url: '/docs/releases/Release5',
+    buttonText: 'Learn More'
   },
   {
-    title: 'Introduction to SCS',
-    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
-    url: '/docs',
-    buttonText: 'Start Now'
+    title: 'Frequently Asked Questions',
+    body: 'You are wondering what is SCS about, what it can do and what it cant?',
+    url: '/docs/faq',
+    buttonText: 'Get Answers'
   },
   {
-    title: 'Deployment Examples',
-    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
-    url: '/docs',
-    buttonText: 'Explore Cases'
+    title: 'Existing Public Clouds',
+    body: 'There are public SCS compliant clouds in production.',
+    url: '/standards/certification/overview#compliant-cloud-environments',
+    buttonText: 'Test them'
   }
 ]
 
 const AdditionalResourcesData = [
   {
-    title: 'Test the Reference Implementation',
-    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
-    url: '/docs',
-    buttonText: 'Get Started'
+    title: 'Get in touch',
+    body: 'Come into our Matrix Chat in the SCS | Tech Room.',
+    url: 'https://matrix.to/#/#scs-tech:matrix.org',
+    buttonText: 'Join Now'
   },
   {
-    title: 'Upgrade Guides',
-    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
-    url: '/docs',
-    buttonText: 'Dive Deeper'
+    title: 'Come to our Meet-Ups',
+    body: 'Our working groups and special interest groups meet weekly or biweekly. When? Find out within our public community calendar.',
+    url: '/community/calendar',
+    buttonText: 'Learn More'
   },
   {
-    title: 'Introduction to SCS',
-    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
-    url: '/docs',
+    title: 'Standardization in progress',
+    body: 'Get to know our current Decision Records and Standards.',
+    url: '/standards',
     buttonText: 'Start Now'
   },
   {
     title: 'Deployment Examples',
-    body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
-    url: '/docs',
+    body: 'Get to know different ways to deploy SCS with cloud resources or on bare metal.',
+    url: '/docs/category/deployment-examples',
     buttonText: 'Explore Cases'
   }
 ]
@@ -109,45 +108,41 @@ export default function Home(): JSX.Element {
           <div className="row" style={{ marginTop: '3rem' }}>
             <div className="col col--12">
               <h1>Architectural Layers</h1>
-              <p>
-                Find user guides, code samples, deployment examples, reference,
-                community pages and more.
-              </p>
             </div>
             <div className={`${styles.gradient} row`}>
               <div className="col col--3">
                 <ContentCard
                   maxHeight
                   title="Ops Layer"
-                  body="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+                  body="Tooling and infrastructure design for easy, efficient and transparent ways to operate an SCS Cloud."
                   buttonText="Learn More"
-                  url="/docs"
+                  url="/docs/category/operating-scs"
                 />
               </div>
               <div className="col col--6">
                 <div style={{ marginBottom: '3rem' }}>
                   <ContentCard
                     title="Container Layer"
-                    body="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+                    body="SCS offers a robust solution for managing container workloads on a Kubernetes infrastructure."
                     buttonText="Learn More"
-                    url="/docs"
+                    url="/docs/container"
                     maxHeight
                   />
                 </div>
                 <ContentCard
                   title="IaaS Layer"
-                  body="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+                  body="SCS offers OpenStack infrastructure solutions based on KVM virutalization to deploy VM workloads and enabling the container layer optionally."
                   buttonText="Learn More"
-                  url="/docs"
+                  url="/docs/category/iaas-layer"
                 />
               </div>
               <div className="col col--3">
                 <ContentCard
                   maxHeight
                   title="IAM Layer"
-                  body="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+                  body="Working on Keycloak federated identity provider within our Team IAM."
                   buttonText="Learn More"
-                  url="/docs"
+                  url="/docs/category/identity-and-access-management-iam"
                 />
               </div>
             </div>
@@ -155,10 +150,6 @@ export default function Home(): JSX.Element {
           <div className="row" style={{ marginTop: '3rem' }}>
             <div className="col col--12">
               <h1>Additional Resources</h1>
-              <p>
-                Find user guides, code samples, deployment examples, reference,
-                community pages and more.
-              </p>
             </div>
           </div>
           <div className="row" style={{ marginBottom: '5rem' }}>
