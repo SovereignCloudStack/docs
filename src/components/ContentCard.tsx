86c35ef7c46a9d79c1a9599d56574c358fbbc1f9
Max Wolfs <mail@maxwolfs.com>
2023-11-08 16:44:14 +0100

    Add Feature Content to Landing Page (#102)
    * Add Content Card Component
    
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>
    
    * Add index page content and layout
    
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>
    
    * fix mobile padding
    
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>
    
    * fix dummy urls
    
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>
    
    * fix links
    
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>
    
    * add content
    
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>
    
    * fix
    
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>
    
    * fix wording
    
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>
    
    ---------
    
    Signed-off-by: Max Wolfs <mail@maxwolfs.com>

---
+++ src/components/ContentCard.tsx
@@ -0,0 +1,33 @@
+import React, { CSSProperties } from 'react'
+import styles from './contentcard.module.css'
+import Link from '@docusaurus/Link'
+
+interface ContentCardProps {
+  title: string
+  body: string
+  url: string
+  buttonText: string
+  maxHeight?: boolean
+}
+
+const ContentCard: React.FunctionComponent<ContentCardProps> = (props) => {
+  const { title, body, url, buttonText, maxHeight } = props
+
+  return (
+    <div className={`${maxHeight && styles.card} card`}>
+      <div className="card__header">
+        <h3>{title}</h3>
+      </div>
+      <div className="card__body">
+        <p>{body}</p>
+      </div>
+      <div className="card__footer">
+        <Link className="button button--secondary button--lg" to={url}>
+          {buttonText}
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default ContentCard
