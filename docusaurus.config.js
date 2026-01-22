6f12a5f5958fb438b93fea5d81da060e565131ee
Kurt Garloff <kurt@garloff.de>
2026-01-22 22:59:55 +0100

    Enable blog and create first article.
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,6 +103,32 @@ const config = {
     './src/plugins/docusaurus-plugin-matomo-analytics/index.js',
     './src/plugins/docusaurus-plugin-global-data/index.js'
   ],
+  blog: {
+    path: './blog',
+    routeBasePath: 'blog',
+    // Author display
+    showReadingTime: true,
+    blogSidebarTitle: 'Recent posts',
+    blogSidebarCount: 10,
+    // Post metadata
+    blogTitle: 'SCS Community Blog',
+    blogDescription:
+      'Insights on cloud sovereignty, open infrastructure, and the Sovereign Cloud Stack',
+    // Authors
+    authorsMapPath: 'authors.yml',
+    // RSS/Atom feeds
+    feedOptions: {
+      type: 'all', // 'rss' | 'atom' | 'json' | 'all'
+      title: 'SCS Community Blog',
+      description: 'Latest articles from the Sovereign Cloud Stack community',
+      copyright: `Copyright © ${new Date().getFullYear()} Sovereign Cloud Stack`,
+      language: 'en'
+    },
+    // Post URLs
+    postsPerPage: 10,
+    // Edit links (important for contributions!)
+    editUrl: 'https://github.com/SovereignCloudStack/docs/edit/main/'
+  },
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
@@ -141,6 +167,11 @@ const config = {
             href: 'https://github.com/SovereignCloudStack/docs',
             label: 'GitHub',
             position: 'right'
+          },
+          {
+            to: '/blog',
+            label: 'Blog',
+            position: 'left'
           }
         ]
       },
