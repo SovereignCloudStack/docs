d40d369f6255192063490b730c6dad8e168afe06
Kurt Garloff <kurt@garloff.de>
2026-01-22 23:51:28 +0100

    Renove old dummy. Add link. Improve wording.
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -199,10 +199,10 @@ const config = {
           {
             title: 'More',
             items: [
-              // {
-              //   label: 'Blog',
-              //   to: '/blog'
-              // },
+              {
+                label: 'Blog',
+                to: '/blog'
+              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/SovereignCloudStack/docs'
