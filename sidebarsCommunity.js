a9b2350bd4d26163ab10d17e83cfec26054b66a4
Matthias Büchse <matthias.buechse@alasca.cloud>
2026-06-05 18:39:35 +0200

    Fix missing comma
    Signed-off-by: Matthias Büchse <matthias.buechse@alasca.cloud>

--- sidebarsCommunity.js
+++ sidebarsCommunity.js
@@ -14,7 +14,7 @@ const sidebars = {
       },
       label: 'Governance',
       items: [
-        'governance/project-board'
+        'governance/project-board',
         'governance/project-board-2025'
       ]
     },
