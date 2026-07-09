37de60a2c0a0eb3e625fb322effd2acb94426bf7
Kurt Garloff <kurt@garloff.de>
2026-07-10 01:06:12 +0200

    Revert .lintstagedrc, drop duplicated process docs.
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- sidebarsStandards.js
+++ sidebarsStandards.js
@@ -24,8 +24,6 @@ const sidebars = {
           label: 'Compliance Check Pipeline',
           id: 'certification/pipeline'
         }
-        'certification/getting-scs-compatible-certified',
-        'certification/test-and-adapt-example',
         'certification/overview'
       ]
     },
